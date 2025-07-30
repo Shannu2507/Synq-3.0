d44ca20c59c89d91db33035cc09096beb16acffd
Shannu2507 <shanmukha8308@gmail.com>
2025-07-30 13:56:18 +0530

    Update supabaseClient.ts

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,9 +1,7 @@
 // src/lib/supabaseClient.ts
 import { createClient } from '@supabase/supabase-js'
 
-const supabase = createClient(
+export const supabase = createClient(
   'https://cdlbntbjvntvmidouydu.supabase.co',
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNkbGJudGJqdm50dm1pZG91eWR1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTIwMDU1NDUsImV4cCI6MjAzNzU4MTU0NX0.Rp3BGUe1TEfYHqucUpxqYosjkHyP8VcbgcYjB8NjVbQ'
 )
-
-export default supabase
