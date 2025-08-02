42e5ac624f75d872965b9c3fc439a45880b4d637
Shannu2507 <shanmukha8308@gmail.com>
2025-08-02 12:09:22 +0530

    feat: add floating post creation button

---
+++ src/app/components/FloatingPostButton.tsx
@@ -0,0 +1,7 @@
+export default function FloatingPostButton() {
+  return (
+    <button className="fixed bottom-8 left-1/2 transform -translate-x-1/2 z-20 bg-gradient-to-r from-cyan-400 to-blue-500 text-white rounded-full w-14 h-14 text-2xl shadow-lg">
+      +
+    </button>
+  )
+}
