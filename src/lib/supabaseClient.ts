2c96d7cbfb996a2b6d8dc445c412959fe12bcc8c
Shannu2507 <shanmukha8308@gmail.com>
2025-08-02 17:25:42 +0530

    Update supabaseClient.ts

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,4 +1,3 @@
-// src/lib/supabaseClient.ts
 import { createClient as createSupabaseClient } from "@supabase/supabase-js";
 
 const supabaseUrl = "https://pyvqjtatbyqxnsmhgixw.supabase.co";
@@ -6,5 +5,4 @@ const supabaseAnonKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB5dnFqdGF0YnlxeG5zbWhnaXh3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTM1MjM5NDEsImV4cCI6MjA2OTA5OTk0MX0.zjdNfe3kF_ZtdlJHfcE1guYbQd5EXeOfqlo6YC0cg8M";
 
 export const supabase = createSupabaseClient(supabaseUrl, supabaseAnonKey);
-export const createClient = () =>
-  createSupabaseClient(supabaseUrl, supabaseAnonKey);
+export const createClient = () => createSupabaseClient(supabaseUrl, supabaseAnonKey);
