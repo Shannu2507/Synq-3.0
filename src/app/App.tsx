6a26d1a2ec2f3957153a0a1480ea14bcdbb87081
Shannu2507 <shanmukha8308@gmail.com>
2025-08-02 12:29:38 +0530

    chore: integrate UserSync into App layout

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,14 +4,16 @@ import TopNav from "./components/TopNav"
 import BottomNav from "./components/BottomNav"
 import FloatingPostButton from "./components/FloatingPostButton"
 import PostFeed from "./components/PostFeed"
+import UserSync from "./components/UserSync"
 
 export default function App() {
   return (
     <div className="bg-black text-white min-h-screen flex flex-col">
       <TopNav />
+      <UserSync />
 
       <main className="flex-1 pb-24 px-4 pt-4 space-y-4">
-        {/* Future: Stories + Trending will go here */}
+        {/* Future: Stories and trending hashtags can go above */}
         <PostFeed />
       </main>
 
