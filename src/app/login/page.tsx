db6e4b6b26bdc60c564b570fb9c358f00dfb5b72
Shannu2507 <shanmukha8308@gmail.com>
2025-08-04 12:58:34 +0530

    Update page.tsx

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,21 +1,11 @@
-// src/app/login/page.tsx
-"use client"
+'use client'
 
-import { supabase } from "@/lib/supabaseClient"
+import Login from '../components/Login'
 
 export default function LoginPage() {
-  const handleSignIn = async () => {
-    await supabase.auth.signInWithOAuth({ provider: "google" })
-  }
-
   return (
     <div className="flex items-center justify-center h-screen bg-black text-white">
-      <button
-        onClick={handleSignIn}
-        className="bg-white text-black px-6 py-3 rounded-xl font-semibold shadow-md hover:bg-gray-200 transition"
-      >
-        Sign in with Google
-      </button>
+      <Login />
     </div>
   )
 }
