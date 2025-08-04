0bd54f6f2cfc63f00e5ac162a3dce6ee5a6662c1
Shannu2507 <shanmukha8308@gmail.com>
2025-08-04 12:51:22 +0530

    Update supabaseClient.ts

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -4,7 +4,6 @@ const supabaseUrl = 'https://pyvqjtatbyqxnsmhgixw.supabase.co'
 const supabaseAnonKey =
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB5dnFqdGF0YnlxeG5zbWhnaXh3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTM1MjM5NDEsImV4cCI6MjA2OTA5OTk0MX0.zjdNfe3kF_ZtdlJHfcE1guYbQd5EXeOfqlo6YC0cg8M'
 
-export const supabase = createSupabaseClient(supabaseUrl, supabaseAnonKey)
-const createClient = () => createSupabaseClient(supabaseUrl, supabaseAnonKey)
+const supabase = createSupabaseClient(supabaseUrl, supabaseAnonKey)
 
-export default createClient
+export default supabase
